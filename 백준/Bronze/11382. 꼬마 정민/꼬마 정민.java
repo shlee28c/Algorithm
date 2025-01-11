9352cc731aa4e2d5e418152a0926c24f8a75395a
shlee28c <156730384+shlee28c@users.noreply.github.com>
2025-01-11 12:28:19 +0900

    [Bronze V] Title: 꼬마 정민, Time: 176 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+      long a=console.nextLong();
+      long b=console.nextLong();
+      long c=console.nextLong();
+      console.close();
+      System.out.println((a+b+c));
+  }
+}

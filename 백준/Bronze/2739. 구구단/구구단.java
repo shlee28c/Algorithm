dfc465ce08151cdf65aa79cd87254ee4e40df08d
shlee28c <156730384+shlee28c@users.noreply.github.com>
2025-01-17 18:21:32 +0900

    [Bronze V] Title: 구구단, Time: 192 ms, Memory: 18564 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+  public static void main (String[] args) {
+    Scanner console = new Scanner (System.in);
+    int a=console.nextInt();
+    for (int i=1; i <=9; i++){
+      System.out.println(a+" * "+i+" = "+i*a);
+    }
+      }
+    }

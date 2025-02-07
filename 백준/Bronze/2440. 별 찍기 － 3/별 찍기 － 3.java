79181a909e757ac2aca46951c96dd1c7d1b953b4
shlee28c <156730384+shlee28c@users.noreply.github.com>
2025-02-07 15:02:25 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 240 ms, Memory: 17992 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+  public static void main (String[] args) {
+    Scanner console = new Scanner (System.in);
+    int s=console.nextInt();
+    for (int i=s; i>=1; i--){
+      for(int j=i; j>=1; j--){
+        System.out.print("*");
+      }
+      System.out.println(" ");
+      }
+    }
+    }
